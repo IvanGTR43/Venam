af1179f1b89c6ea3d03da3900f3b8652c7d63384
IvanGTR <ivan_tr43@outlook.com>
2020-10-25 19:43:04 -0600

    Initial commit

---
+++ app/src/main/java/com/ivangtr/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ivangtr.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintLayout
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
